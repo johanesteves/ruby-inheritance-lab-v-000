74854d7b1d5136303be9199400475215014eff45
Johan Esteves <jsesteves91@gmail.com>
2017-09-11 03:40:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,14 @@
 require_relative "./user.rb"
 
 class Student < User
+  attr_reader :knowledge
+  
   def initalize
     @knowledge = []
   end
 
   def learn(text)
     @knowledge << text
-
   end
+
 end
